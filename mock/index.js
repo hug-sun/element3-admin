75b1800286facd47d7f1bfa034bbc3cb94e9e884
peter <283776805@qq.com>
2020-12-02 16:07:27 +0800

    添加mock数据

--- mock/index.js
+++ mock/index.js
@@ -3,10 +3,16 @@ const { param2Obj } = require('./utils')
 
 const user = require('./user')
 const table = require('./table')
+const role = require('./role')
+const article = require('./article')
+const search = require('./remote-search')
 
 const mocks = [
   ...user,
-  ...table
+  ...table,
+  ...role,
+  ...article,
+  ...search
 ]
 
 // for front mock
--- src/api/table.js
+++ src/api/table.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function getList(params) {
   return request({
-    url: '/vue-admin-template/table/list',
+    url: '/vue-element-admin/table/list',
     method: 'get',
     params
   })
