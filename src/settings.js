12f61a546655142124c4fed3eab782fc0c19d737
peter <283776805@qq.com>
2020-12-03 16:04:33 +0800

    change router && icon

--- src/settings.js
+++ src/settings.js
@@ -3,6 +3,12 @@ module.exports = {
 
   /**
    * @type {boolean} true | false
+   * @description Whether show the settings right-panel
+   */
+  showSettings: true,
+
+  /**
+   * @type {boolean} true | false
    * @description Whether fix the header
    */
   fixedHeader: false,
@@ -18,4 +24,12 @@ module.exports = {
    * @description Whether need tagsView
    */
   tagsView: true,
+
+  /**
+   * @type {string | array} 'production' | ['production', 'development']
+   * @description Need show err logs component.
+   * The default is only used in the production env
+   * If you want to also use it in dev, you can pass ['production', 'development']
+   */
+  errorLog: 'production'
 }
