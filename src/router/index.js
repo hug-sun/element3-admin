e2cbb72d3ffa8bdc319232d5b8038e51501486c1
peter <283776805@qq.com>
2020-12-10 11:52:03 +0800

    add nestedRouter

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ import Layout from '@/layout'
 import componentsRouter from './modules/components'
 import chartsRouter from './modules/charts'
 // import tableRouter from './modules/table'
-// import nestedRouter from './modules/nested'
+import nestedRouter from './modules/nested'
 
 /**
  * Note: sub-menu only appear when route children.length >= 1
@@ -133,7 +133,8 @@ export const constantRoutes = [
   },
   // todo 应放在异步路由，权限暂没弄好，先在这里测试
   componentsRouter,
-  chartsRouter
+  chartsRouter,
+  nestedRouter
 ]
 
 /**
@@ -183,22 +184,22 @@ export const asyncRoutes = [
   //   ]
   // },
 
-  {
-    path: '/icon',
-    component: Layout,
-    children: [
-      {
-        path: 'index',
-        component: () => import('@/views/icons/index'),
-        name: 'Icons',
-        meta: { title: 'Icons', icon: 'icon', noCache: true },
-      },
-    ],
-  },
+  // {
+  //   path: '/icon',
+  //   component: Layout,
+  //   children: [
+  //     {
+  //       path: 'index',
+  //       component: () => import('@/views/icons/index'),
+  //       name: 'Icons',
+  //       meta: { title: 'Icons', icon: 'icon', noCache: true },
+  //     },
+  //   ],
+  // },
 
   // /** when your routing map is too long, you can split it into small modules **/
-  componentsRouter,
-  chartsRouter,
+  // componentsRouter,
+  // chartsRouter,
   // nestedRouter,
   // tableRouter,
 
