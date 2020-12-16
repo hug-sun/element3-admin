17bb95a1d0ecf65b2c0417094f1ee408afa67a69
wxy <wxy306533309@163.com>
2020-12-16 16:17:23 +0800

    添加Error Pages功能

--- src/router/index.js
+++ src/router/index.js
@@ -180,6 +180,30 @@ export const constantRoutes = [
         }
       ]
     },
+    {
+        path: '/error',
+        component: Layout,
+        redirect: 'noRedirect',
+        name: 'ErrorPages',
+        meta: {
+          title: 'Error Pages',
+          icon: '404'
+        },
+        children: [
+          {
+            path: '401',
+            component: () => import('@/views/error-page/401'),
+            name: 'Page401',
+            meta: { title: '401', noCache: true }
+          },
+          {
+            path: '404',
+            component: () => import('@/views/error-page/404'),
+            name: 'Page404',
+            meta: { title: '404', noCache: true }
+          }
+        ]
+      },
 ]
 
 /**
