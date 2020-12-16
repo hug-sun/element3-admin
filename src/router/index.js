ae63ac066f665957d41168079c862ab6a8feb4b9
peter <283776805@qq.com>
2020-12-16 18:02:37 +0800

    add clipboard

--- src/router/index.js
+++ src/router/index.js
@@ -217,6 +217,18 @@ export const constantRoutes = [
           ]
       },
       {
+        path: '/clipboard',
+        component: Layout,
+        children: [
+          {
+            path: 'index',
+            component: () => import('@/views/clipboard/index'),
+            name: 'ClipboardDemo',
+            meta: { title: 'Clipboard', icon: 'clipboard' }
+          }
+        ]
+      },
+      {
         path: '/external-link',
         component: Layout,
         children: [
